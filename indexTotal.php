e69af99a05503f12820b3ac96b9646566717472d
Beiwen Liu <bliu85@gatech.edu>
2016-04-11 01:19:01 -0400

    Successfully finds latest time and corresponding number of people

--- indexTotal.php
+++ indexTotal.php
@@ -1,25 +1,31 @@
 <?php
 
 function retrieveLatestTime($date) {
-    $found = false;
-    $answer = null;
     $counter = 0;
     require('include/db.php');
     require('include/inputFormHelper.php');
     $conn = new mysqli($servername, $username, $password, $dbname);
     $select = "SELECT `People`, `Time` FROM `sheet` WHERE `Date`='$date' AND `Zone`='Time'";
     $row = $conn->query($select);
+    
     $newTimesReversed = array_reverse($newTimes);
+    $key = count($newTimesReversed);
+    $answer = null;
     while($rows = $row->fetch_assoc()) {
-        echo $rows['Time'];
-       while (!$found) {
-           if ($rows['Time'] == $newTimesReversed[$counter]) {
-               $answer = $rows['People'];
-               $found = true;
-           } else {
-               $counter++;
-           }
-       }
+        $temp = array_search($rows['Time'],$newTimesReversed);
+        if ($temp < $key) {
+            $key = $temp;
+            $answer = $rows['People'];
+        }
+//        echo $rows['Time'];
+//       while (!$found) {
+//           if ($rows['Time'] == $newTimesReversed[$counter]) {
+//               $answer = $rows['People'];
+//               $found = true;
+//           } else {
+//               $counter++;
+//           }
+//       }
     }
     return $answer;
 }
