d870b58522aa86b91d17a032c5a59a839aff1733
Beiwen Liu <bliu85@gatech.edu>
2016-04-11 23:43:13 -0400

    Minor fix to array

--- include/inputFormHelper.php
+++ include/inputFormHelper.php
@@ -1,7 +1,7 @@
 <?php
 $titles=array("Time", "Signature", "L. Dumbbell", "SYNRGY/ABS", "Racquetball", "Stretch Mats", "Bikes", "Steppers", "Treadmills", "Elipticals", "Smith/DAP", "Squat", "H. Dumbbell", "Plate Loaded", "MTS");
 
-$times = array("6:30 AM","7:30 AM","8:30 AM","9:30 AM","10:30 AM","11:30 AM","12:30 PM","1:30 PM","2:30 PM","3:30 PM","4:30  PM","5:30 PM","7:30 PM","8:30 PM","9:30 PM","10:30 PM","11:30 PM");
+$times = array("6:30 AM","7:30 AM","8:30 AM","9:30 AM","10:30 AM","11:30 AM","12:30 PM","1:30 PM","2:30 PM","3:30 PM","4:30  PM","5:30 PM","6:30 PM","7:30 PM","8:30 PM","9:30 PM","10:30 PM","11:30 PM");
 
 
 
